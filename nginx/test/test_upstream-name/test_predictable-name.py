08b5187cf169c7251c39d2d71172eeccdf76f6ca
hushengs <xingcheng.hu@androidmov.com>
2021-09-15 17:19:04 +0800

    添加golang

--- nginx/test/test_upstream-name/test_predictable-name.py
+++
@@ -1,7 +0,0 @@
-import pytest
-import re
-
-
-def test_predictable_upstream_is_present_in_nginx_generated_conf(docker_compose, nginxproxy):
-    conf = nginxproxy.get_conf().decode('ASCII')
-    assert re.search(r"upstream web\.nginx-proxy\.tld \{", conf)
